04104909432e538c60f8c9960aa3dbe59ba96aaa
RyanGouldsmith <GouldsmithRyan@hotmail.co.uk>
2014-08-13 20:10:02 +0100

    added a bit of error handling

--- main.py
+++ main.py
@@ -8,5 +8,9 @@ def work_out_sequence(term):
     else: return work_out_sequence(term-1)+ work_out_sequence(term-2)
 
 if __name__ == "__main__":
-    for i in range (0, 20):
-        print (work_out_sequence(i))
+    total = 0
+    if (int (sys.argv[1]) > int(sys.argv[2])):
+        for i in range (0, 20):
+           total += work_out_sequence(i)
+           print (work_out_sequence(i))
+    print "Total sum of your lucas numbers are: %d" % total
